1e77931ad585f9ab7baf9eb9abdee7ebe13de8e4
Hyram Tang <hyram.tang@gmail.com>
2023-09-14 23:29:46 +0800

    feat: add modal component demo

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import "@babel/polyfill";
-import "mutationobserver-shim";
 import Vue from "vue";
-import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import "./plugins/bootstrap-vue";
 
 Vue.config.productionTip = false;
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <b-form-group>
+      <b-button>按钮展示</b-button>
+      <b-button variant="danger">Button</b-button>
+      <b-button variant="success">Button</b-button>
+      <b-button variant="outline-primary">Button</b-button>
+    </b-form-group>
+    <b-form-group>
+      <b-button variant="success" @click="showModal = true"
+        >显示模态框</b-button
+      >
+      <b-button variant="primary" @click="$bvModal.show('bv-modal-example')"
+        >实例方法调用模态框</b-button
+      >
+    </b-form-group>
+    <b-modal id="bv-modal-example" v-model="showModal" title="BootstrapVue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+
+  methods: {},
+};
+</script>
+
+<style lang="less">
+.btn + .btn {
+  margin-left: 8px;
+}
+</style>
